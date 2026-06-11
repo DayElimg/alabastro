b5163d0ab01e41be49473e40ef55792219ae0cb4
DayElimg <Dayleeneli@gmail.com>
2026-06-10 20:52:58 -0400

    Asentamiento base
    Creación de carpetas básicas y dependencias

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Paleta extraída de la identidad visual de Alabastro
+  static const Color backgroundLight = Color(0xFFF8F3ED); 
+  static const Color surfaceCream = Color(0xFFEDEADE);    
+  static const Color primaryGreen = Color(0xFF8A9283);    
+  static const Color darkOlive = Color(0xFF55604C);       
+}
